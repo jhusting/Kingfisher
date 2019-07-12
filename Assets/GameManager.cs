218d6afd3d70d54603e9b0da2f0640c2aac25425
dbirtola <dbirtola@gmail.com>
2019-07-12 16:53:51 -0600

    Main menu and transition to gameplay

---
+++ Assets/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager gameManager { get; private set;}
+
+    public PlayerController playerController { get; private set; }
+
+    public Canvas gameHUD;
+    public Canvas menuHUD;
+
+    void Awake()
+    {
+        //Ensure this is a singleton
+        if(gameManager != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameManager = this;
+        }
+    }
+
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    public void StartDive()
+    {
+
+        playerController.StartCoroutine(playerController.DiveIn(2f));
+        menuHUD.gameObject.SetActive(false);
+        gameHUD.gameObject.SetActive(true);
+
+    }
+
+    public void BackToMenu()
+    {
+        menuHUD.gameObject.SetActive(true);
+        gameHUD.gameObject.SetActive(false);
+    }
+}
