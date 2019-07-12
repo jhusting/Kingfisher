c78213c6d8dc2bdf3698efa58cfbcd227de9d2bc
jhusting <joshhusting@gmail.com>
2019-07-11 23:43:09 -0700

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // The refence to the spear
+    public GameObject spear;
+    public GameObject gun;
+    public LineRenderer rope;
+    public GameObject spearEnd;
+
+    public float moveSpeed = 1f;
+
+    public float spearMaxDistance = 5f;
+    public float spearStrength = 0f;
+
+    [SerializeField]
+    private float baseMoveSpeed = 1f;
+    [SerializeField]
+    private float burstMovementModifier = 2f;
+
+    [SerializeField]
+    private float maxOxygen = 10f;
+    [SerializeField]
+    private float oxygenBurnRate = 1f;
+    private float currOxygen = 0f;
+
+    [SerializeField]
+    private AnimationCurve burstCurve;
+
+    private bool spearShot = false;
+    private bool spearReturned = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currOxygen = maxOxygen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateToMouse(gun);
+
+        if (spearReturned && !spearShot)
+            RotateToMouse(spear);
+        else
+        {
+            Vector3[] pos = new Vector3[2];
+            pos[0] = Vector3.zero;
+            pos[1] = spearEnd.transform.position - rope.transform.position;
+            rope.SetPositions(pos);
+        }
+
+        ReadInput();
+    }
+
+    void RotateToMouse(GameObject g)
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - g.transform.position;
+        diff.Normalize();
+
+        // Gets the angle from the mouse position
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        g.transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+        //currWind.Size = (currWind.StartPos - mouseLocation).magnitude + .4f;
+    }
+
+    void ReadInput()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            StartCoroutine("InhaleBurst");
+            StartCoroutine("SpeedBurst");
+            //currOxygen = Mathf.Clamp(currOxygen - 2f, 0f, maxOxygen);
+        }
+
+        if(Input.GetKeyUp("space"))
+        {
+            StartCoroutine("SpeedBurst");
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (!spearShot)
+            {
+                StartCoroutine("SpearCharge");
+            }
+            else
+                StartCoroutine("SpearReturn");
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if (!spearShot)
+            {
+                Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - spear.transform.position;
+                Rigidbody2D spearRB = spear.GetComponent<Rigidbody2D>();
+
+                spearRB.AddForce((spearMaxDistance * 400f) * spearStrength * direction.normalized);
+                spearRB.gravityScale = 0.2f;
+
+                //rope.gameObject.SetActive(true);
+                spearStrength = 0f;
+                spearShot = true;
+                spearReturned = false;
+            }
+            else
+            {
+                spearShot = false;
+            }
+        }
+    }
+
+    public float GetOxygenPercent()
+    {
+        return Mathf.Clamp(currOxygen / maxOxygen, 0f, 1f);
+    }
+
+    IEnumerator InhaleBurst()
+    {
+        float time = 0f;
+        while (Input.GetKey("space"))
+        {
+            currOxygen = Mathf.Clamp(currOxygen - oxygenBurnRate * burstCurve.Evaluate(time), 0f, maxOxygen);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator SpeedBurst()
+    {
+        float time = 0f;
+        while (time < 0.4f)
+        {
+            moveSpeed = baseMoveSpeed + burstMovementModifier * burstCurve.Evaluate(time / 0.4f);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator SpearCharge()
+    {
+        float time = 0f;
+
+        while(Input.GetMouseButton(0) && (time < 0.7f || spearStrength >= 1f))
+        {
+            spearStrength = Mathf.Clamp(time / 0.7f, 0f, 1f);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator SpearReturn()
+    {
+        Vector3 currPos = spear.transform.localPosition;
+        float reelSpeed = 5f;
+
+        while(Vector3.Distance(currPos, Vector3.zero) > reelSpeed*Time.deltaTime)
+        {
+            Vector3 moveDirectionNormalized = (-1f * currPos).normalized;
+            currPos = currPos + moveDirectionNormalized * reelSpeed * Time.deltaTime;
+            spear.transform.localPosition = currPos;
+
+            yield return null;
+        }
+
+        spear.transform.localPosition = Vector3.zero;
+
+        Rigidbody2D spearRB = spear.GetComponent<Rigidbody2D>();
+        spearRB.gravityScale = 0f;
+        spearRB.velocity = Vector3.zero;
+        spearReturned = true;
+        //rope.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Slider oxySlider;
+    private RectTransform oxyRect;
+
+    public Slider strengthSlider;
+    public Text currSpeed;
+
+    public PlayerController pc;
+
+    [SerializeField]
+    private AnimationCurve oxyShakeStrength;
+    [SerializeField]
+    private AnimationCurve shakeCurve;
+    private float waitTime = 0.8f;
+    private bool shaking = false;
+
+    private Vector2 oxyStartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oxyRect = oxySlider.GetComponent<RectTransform>();
+        oxyStartPos = oxyRect.anchoredPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oxySlider.value = pc.GetOxygenPercent();
+        currSpeed.text = "" + pc.moveSpeed;
+
+        if (pc.spearStrength > 0f)
+        {
+            //strengthSlider.colors.
+            strengthSlider.value = pc.spearStrength;
+        }
+        else
+            strengthSlider.value = 0f;
+
+        OxygenTick();
+    }
+
+    void OxygenTick()
+    {
+        float strength = oxyShakeStrength.Evaluate(1f - oxySlider.value);
+
+        if(!shaking)
+            StartCoroutine(OxyShake(0.3f*strength, strength));
+    }
+
+    IEnumerator OxyShake(float shakeTime, float strength)
+    {
+        float time = 0;
+        shaking = true;
+
+        for (; time < shakeTime; time += Time.deltaTime)
+        {
+            float newX = oxyStartPos.x + (shakeCurve.Evaluate(time / shakeTime) - 0.5f) * strength * 6f;
+
+            oxyRect.anchoredPosition = new Vector2(newX, oxyStartPos.y);
+
+            yield return null;
+        }
+
+        for (time = 0; time < waitTime; time += Time.deltaTime)
+            yield return null;
+
+        shaking = false; 
+    }
+}
