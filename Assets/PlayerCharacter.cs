b430c3070a6474c7ec73aa23a8625ab4dbf7fb5c
dbirtola <dbirtola@gmail.com>
2019-07-12 15:30:01 -0600

    Vertical movement

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private int speed;
+
+    PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Get the vertical speed from the player controller
+
+        float verticalSpeed = 1f;
+        transform.position += Vector3.up * Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
+
+
+    }
+}
